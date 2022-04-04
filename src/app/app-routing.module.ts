98ec98719b4822ca82d9e697e33ab6284ab1f088
JesusLC96 <jesuslc96@gmail.com>
2022-04-03 22:13:20 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
   {path: 'nosotros', component: NosotrosComponent},
   {path: 'servicios', component: ServiciosComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: '',redirectTo: 'inicio', pathMatch: 'full'},
 ];
 
 @NgModule({
