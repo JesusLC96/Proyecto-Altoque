3d33e2752d01a647671c6279f5fcdda2985683ee
Christian Quispe <chrisalexqm@gmail.com>
2022-04-04 13:26:16 -0500

    vistas: inicio y servicios

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  menu = [
+    {
+      name: 'Inicio',
+      route: '/inicio',
+    },
+    {
+      name: 'Nosotros',
+      route: '/nosotros',
+    },
+    {
+      name: 'Servicios',
+      route: '/servicios',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
+  styleUrls: ['./servicios.component.css'],
 })
 export class ServiciosComponent implements OnInit {
+  menu = [
+    {
+      name: 'Inicio',
+      route: '/inicio',
+    },
+    {
+      name: 'Nosotros',
+      route: '/nosotros',
+    },
+    {
+      name: 'Servicios',
+      route: '/servicios',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
